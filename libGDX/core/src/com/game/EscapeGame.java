6e71aebf21b3c28a37502c0b2cc30a4bb1a9e436
Matthew d'Entremont <mldentremont@mun.ca>
2020-02-10 12:36:05 -0330

    added camera config
    Added camera/window configs and obviously motion and murr.

--- libGDX/core/src/com/game/EscapeGame.java
+++ libGDX/core/src/com/game/EscapeGame.java
@@ -3,31 +3,50 @@ package com.game;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
-public class EscapeGame extends ApplicationAdapter {
+public class escapeGame extends ApplicationAdapter {
+
+	public static int WIDTH;
+	public static int HEIGHT;
+
+	public static OrthographicCamera cam;
+
 	SpriteBatch batch;
-	Texture img;
+
+	Texture motion;
+
 	
 	@Override
 	public void create () {
+
+		WIDTH = Gdx.graphics.getWidth();
+		HEIGHT = Gdx.graphics.getHeight();
+
+		cam = new OrthographicCamera(WIDTH,HEIGHT);
+		cam.translate(WIDTH/2,HEIGHT/2);
+		cam.update();
+
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		motion = new Texture("jamil.jpg");
+
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+		//Clear Screen to black then open textures.
+		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		batch.draw(img, 0, 0);
+		batch.draw(motion, WIDTH/2, HEIGHT/2);
 		batch.end();
 	}
 	
 	@Override
 	public void dispose () {
 		batch.dispose();
-		img.dispose();
+		motion.dispose();
 	}
 }
