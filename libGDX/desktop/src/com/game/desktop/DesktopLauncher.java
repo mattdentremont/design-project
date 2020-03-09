7aa10056895f785368c909cd4bdbe586e50a32f2
Matthew d'Entremont <mldentremont@mun.ca>
2020-03-09 15:01:11 -0230

    Changed app config
    changed to windowed mode, added app icon.

--- libGDX/desktop/src/com/game/desktop/DesktopLauncher.java
+++ libGDX/desktop/src/com/game/desktop/DesktopLauncher.java
@@ -1,5 +1,6 @@
 package com.game.desktop;
 
+import com.badlogic.gdx.Files;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.game.escapeGame;
@@ -8,9 +9,11 @@ public class DesktopLauncher {
 	public static void main (String[] arg) {
 
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.title = "Dungeon Escape";
 		config.width = 1920;
 		config.height=1080;
-		config.fullscreen = true;
+		config.fullscreen = false;
+		config.addIcon("jamilIcon.jpg", Files.FileType.Internal);
 		new LwjglApplication(new escapeGame(), config);
 	}
 }
