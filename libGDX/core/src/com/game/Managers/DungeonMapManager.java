829bd69d96c7df7a06252d0cd0ff8d2d9929b9e4
ldavidk <lukedavidkelly@gmail.com>
2020-04-06 19:52:12 -0230

    commnted Dungeon Map manager

--- libGDX/core/src/com/game/Managers/DungeonMapManager.java
+++ libGDX/core/src/com/game/Managers/DungeonMapManager.java
@@ -19,15 +19,21 @@ public class DungeonMapManager {
         this.width = x;
         this.height = y;
         this.mapList = maps;
-        dungeon = new Room[x][y];
+
+        //Create new dungeon of size X * Y and populate with rooms and maps
+        this.dungeon = new Room[x][y];
         for (int i = 0; i < width; i++) {
                 for(int j = 0; j < height; j++){
                 int rand = new Random().nextInt(mapList.length);
-                dungeon[i][j] = new Room(mapList[rand]);//assign random map to each dungeon room.
+                this.dungeon[i][j] = new Room(mapList[rand]);//assign random map to each dungeon room.
             }
         }
+
+        //Start in middle of the matrix
         this.xPos = x/2;
         this.yPos = y/2;
+
+        //Set DVDemon Boss to appear in specific room
         int randxDVD = new Random().nextInt(x);
         int randyDVD = new Random().nextInt(y);
         while(xPos == randxDVD && yPos == randyDVD)//avoid dvdemon in first room
@@ -35,6 +41,9 @@ public class DungeonMapManager {
              randxDVD = new Random().nextInt(x);
              randyDVD = new Random().nextInt(y);
         }
+        dungeon[randxDVD][randyDVD].isDVDemon = true;
+
+        //Set VHDL Boss to appear in specific room
         int randxVHDL = new Random().nextInt(x);
         int randyVHDL = new Random().nextInt(y);
         while(xPos == randxVHDL && yPos == randyVHDL ||randxDVD == randxVHDL && randyDVD == randyVHDL )//avoid first room & duplicate boss
@@ -42,21 +51,14 @@ public class DungeonMapManager {
             randxVHDL = new Random().nextInt(x);
             randyVHDL= new Random().nextInt(y);
         }
-        dungeon[randxDVD][randyDVD].isDVDemon = true;
         dungeon[randxVHDL][randyVHDL].isVHDL = true;
-        //dungeon[randxVHDL][randyVHDL].isProjectileEnemy = true;
+
+        //Start In Middle Room
         setCurrentRoom(xPos,yPos,true);
         currentRoom.setHasBeenVisited();
     }
 
-    public Room[][] getDungeon() {
-        return dungeon;
-    }
-
-    public String[] getMaps() {
-        return mapList;
-    }
-
+    //Some Accessors
     public Room getCurrentRoom() {
         return currentRoom;
     }
@@ -80,26 +82,34 @@ public class DungeonMapManager {
         return this.yPos;
     }
 
+    //Change rooms when player enters a new one
+    //X and Y are position in dungeonMap Matrix
     public void setCurrentRoom(int x, int y,boolean isFirst)
     {
-        if(x>=0 && x < this.width && y>=0 && y < this.height) {
+        if(x>=0 && x < this.width && y>=0 && y < this.height) //If within dungeon matrix
+        {
             if(currentRoom != null)
-            currentRoom.setHasBeenVisited();
-            currentRoom = dungeon[x][y];
-            this.xPos = x;
-            this.yPos = y;
-            if(isFirst == true)
             {
                 currentRoom.setHasBeenVisited();
             }
-            if(currentRoom.hasBeenVisited == false)
+
+            currentRoom = dungeon[x][y];
+            this.xPos = x;
+            this.yPos = y;
+
+            if(isFirst == true) //If this is the first/spawn room
+            {
+                currentRoom.setHasBeenVisited();
+            }
+
+            if(currentRoom.hasBeenVisited == false) //first time entering a new room
             {
                 player.incrementScore(50);
                 player.incrementRoomsVisited();
-                System.out.println("Score: "+ player.getScore() + " Health: "+ player.health+ " Rooms Visited: "+ player.getRoomsVisited());
+                System.out.println("Score: " + player.getScore() + " Health: " + player.health + " Rooms Visited: " + player.getRoomsVisited());
             }
         }
-        else return;
+        else return; //Not Within Map Matrix, Return
     }
 
 
